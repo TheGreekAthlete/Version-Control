920eb460c73e1e303a24c4c525f24d977f6ef0a4
Lucas Gavrielides <lgavrielides@ufl.edu>
2023-10-26 16:45:04 -0400

    Fixed the logic behind password (works for 0-9), aswell as added pritning logic for that.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 
 def encode(password):
-    return ''.join([str(int(item) + 3) for item in password])
+    return ''.join([str((int(item) + 3) % 10) for item in password])
 
 # logic to decode the encoded string once provided
 def decode(password):
-    return ''.join([str(int(item) - 3) for item in password])
+    return ''.join([str((int(item) - 3) % 10) for item in password])
 
 # menu/user prompting
 
@@ -19,9 +19,13 @@ while True:
 
     if option == 1:
         original_password = input('Please enter your password to encode: ')
-        encoded_password = encode(original_password)
-        print('Your password has been encoded and stored!\n')
-        continue
+        if len(original_password) == 8 and original_password.isnumeric():
+            encoded_password = encode(original_password)
+            print('Your password has been encoded and stored!\n')
+            continue
+        else:
+            print('Invalid Input! Password contains more than 8 digits or contains a letter! \n ')
+
     if option == 2:
         print(f'The encoded password is {encoded_password}, and the original password is {original_password}.\n')
     if option == 3:
